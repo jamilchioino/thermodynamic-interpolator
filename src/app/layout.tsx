c9e7318d0439db261acd820adc7dc1b4ffbe7eb3
jamilchioino <chiono@gmail.com>
2024-09-29 21:09:16 -0500

    feat: add google analytics for tracking page views

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId={process.env.GA_ID ?? ""} />
         <Toaster />
       </body>
     </html>
